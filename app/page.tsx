9d82381c779e1f90750567c62172fb313f55e998
yugojyoti <yugojyotimohanta@gmail.com>
2023-10-13 01:28:31 +0530

    "build changes"

--- app/page.tsx
+++ app/page.tsx
@@ -17,38 +17,46 @@ export interface RestaurantCardType {
 
 const prisma = new PrismaClient();
 
-const fetchRestaurants = async (): Promise<RestaurantCardType[]> => {
-  const restaurants = await prisma.restaurant.findMany({
-    select: {
-      id: true,
-      name: true,
-      main_image: true,
-      cuisine: true,
-      slug: true,
-      location: true,
-      price: true,
-      reviews: true,
-      open_time: true,
-      close_time: true,
-    },
-  });
+const fetchRestaurants = async () => {
+  try {
+    const restaurants = await prisma.restaurant.findMany({
+      select: {
+        id: true,
+        name: true,
+        main_image: true,
+        cuisine: true,
+        slug: true,
+        location: true,
+        price: true,
+        reviews: true,
+        open_time: true,
+        close_time: true,
+      },
+    });
 
-  return restaurants;
+    return restaurants;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 export default async function Home() {
   const restaurants = await fetchRestaurants();
 
   return (
-    <main>
-      <Header />
-      <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
-        {restaurants.map((restaurant) => (
-          <div key={restaurant.id}>
-            <RestaurantCard restaurant={restaurant} />
+    <>
+      {restaurants && (
+        <main>
+          <Header />
+          <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
+            {restaurants.map((restaurant) => (
+              <div key={restaurant.id}>
+                <RestaurantCard restaurant={restaurant} />
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-    </main>
+        </main>
+      )}
+    </>
   );
 }
